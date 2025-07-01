fc5a01dd27aca011a380e7aaae5b99d6f7424aac
Vishalsingh2428 <vishal.240180107059@gmail.com>
2025-07-01 12:20:04 +0530

    Day 2 and 3 added

---
+++ DAY2(30 June)/Addoftwono.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,r;
+    cout<<"Enter First Number: ";
+    cin >> a;
+    cout<<"Enter Second Number: ";
+    cin>> b;
+    r=a+b;
+    cout<<"The Sum of "<< a << " and"<< b << " is " << r <<endl;
+
+
+    return 0;
+}
---
+++ DAY3(1 July)/areaofreactangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l,b,a;
+    cout<<"Enter Length of Rectangle:";
+    cin>>l;
+    cout<<"Enter Breadth Of Rectangle:";
+    cin>>b;
+    a=l*b;
+    cout<<"The Area Of Reactangle with Length:"<<l << " and Breadth:"<<b<<" is "<<a<<endl;
+    return 0;
+} 
